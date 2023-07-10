6a7c5836c0c43c162cd20d44686a7eb985eaab73
Joao Manuel Lima Lomba <jlima-lo@c1r11s1.42porto.com>
2023-07-10 13:44:48 +0100

    m

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlima-lo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 16:24:39 by jlima-lo          #+#    #+#             */
+/*   Updated: 2023/07/07 16:24:41 by jlima-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef OPEN_FMAX
+#  define OPEN_FMAX 4096
+# endif 
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*read_gnl(int fd, char *buff);
+int		find_ln(char *buff);
+char	*find_put_ln(char *buff);
+char	*take_ln(char *buff);
+
+#endif
